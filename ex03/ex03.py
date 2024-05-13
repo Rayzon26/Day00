c93767cb943632da70cbedf5de3c4208430356be
Rayzon26 <theotime.rey@gmail.com>
2024-05-13 12:39:07 +0200

    ajout ex03

---
+++ ex03/ex03.py
@@ -0,0 +1,16 @@
+def print_reverse_alphabet():
+
+    start = ord('z')
+    
+
+    end = ord('a')
+    
+ 
+    for i in range(start, end - 1, -1):
+     
+        letter = chr(i)
+      
+        print(letter)
+
+
+print_reverse_alphabet()
