85eee7da90935222f99838af86453ef1c18c679e
Rayzon26 <theotime.rey@gmail.com>
2024-05-13 12:35:35 +0200

    ajout ex02

---
+++ ex02/ex02.py
@@ -0,0 +1,16 @@
+def print_alphabet():
+   
+    start = ord('a')
+    
+   
+    end = ord('z')
+    
+   
+    for i in range(start, end + 1):
+       
+        letter = chr(i)
+        
+        print(letter)
+
+
+print_alphabet()
